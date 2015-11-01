4017c42933ca824c13d4775d777ee957b964a994
Ricardo Bevilacqua <ricardob.redmond@gmail.com>
2015-11-01 15:22:49 -0300

    Basic squeleton with fragments

---
+++ app/src/main/java/com/test/ricardobevi/contactsapp/view/activities/ContactActivity.java
@@ -0,0 +1,15 @@
+package com.test.ricardobevi.contactsapp.view.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.test.ricardobevi.contactsapp.R;
+
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+}
---
+++ app/src/main/java/com/test/ricardobevi/contactsapp/view/fragments/ContactListFragment.java
@@ -0,0 +1,9 @@
+package com.test.ricardobevi.contactsapp.view.fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by ric on 01/11/15.
+ */
+public class ContactListFragment  extends Fragment {
+}
