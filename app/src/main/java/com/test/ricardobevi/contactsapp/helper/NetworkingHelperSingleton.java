d9ca959f4c4849cf24022af66cc65c5b5bb5b8e5
Ricardo Bevilacqua <ricardob.redmond@gmail.com>
2015-11-02 00:58:37 -0300

    Added volley framework and singleton helper to use it

---
+++ app/src/main/java/com/test/ricardobevi/contactsapp/helper/NetworkingHelperSingleton.java
@@ -0,0 +1,69 @@
+package com.test.ricardobevi.contactsapp.helper;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by ric on 01/11/15.
+ */
+public class NetworkingHelperSingleton {
+
+    private static NetworkingHelperSingleton sInstance;
+    private static Context mContext;
+
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+
+
+    private NetworkingHelperSingleton(Context context) {
+        mContext = context;
+        mRequestQueue = getRequestQueue();
+
+        mImageLoader = new ImageLoader(mRequestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap>
+                            cache = new LruCache<String, Bitmap>(20);
+
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
+    }
+
+    public static synchronized NetworkingHelperSingleton getInstance(Context context) {
+        if (sInstance == null) {
+            sInstance = new NetworkingHelperSingleton(context);
+        }
+        return sInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            mRequestQueue = Volley.newRequestQueue(mContext.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImageLoader() {
+        return mImageLoader;
+    }
+}
+
